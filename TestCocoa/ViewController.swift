e8721a47f4c003b6ee6114f7f5973fcac733167b
Liuyubao <523331232@qq.com>
2017-12-21 18:06:18 +0800

    sdfd

--- TestCocoa/ViewController.swift
+++ TestCocoa/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var testStr = "handsome"
+        testStr = testStr.getLiuyubaoString(inText: "hello")
+        NSLog(testStr)
+        print(testStr)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ liuyubaoClasses/String+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  String+Extension.swift
+//  TestCocoa
+//
+//  Created by 柳玉豹 on 2017/12/21.
+//  Copyright © 2017年 xinghaiwulian. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func getLiuyubaoString(inText:String)->String{
+        return "liuyubao"+inText
+    }
+
+}
